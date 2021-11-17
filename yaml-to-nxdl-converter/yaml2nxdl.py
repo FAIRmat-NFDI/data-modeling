520db935410c71b67383727b75e852ca4a567201
Markus Kuehbach <kuehbach@fhi-berlin.mpg.de>
2021-11-17 10:30:16 +0000

    checked correct functioning of the parser against NXarpes, NXmx will be tested next, including the stylesheet line and the xsi schema attributes in the definitions headers are missing

--- yaml-to-nxdl-converter/yaml2nxdl.py
+++ yaml-to-nxdl-converter/yaml2nxdl.py
@@ -13,12 +13,14 @@ from lxml import etree
 
 def nx_base_clss_string_mangling(tmp):
     if tmp[0:3] == 'nx_':
-        return 'NX_'+tmp[3::]
+        return 'NX'+tmp[3::]
     else:
         return tmp
         #raise ValueError(tmp+' is not a properly formatted nexus keyword which this parser knows!')
 
-fnm = '/home/kuehbach/HU_HU_HU/FAIRmatSoftwareDevelopment/Sprint03_EmApplicationDefinition/issue_xx_yaml2nxdl/NXmx.yml'
+fnm = '/home/kuehbach/HU_HU_HU/FAIRmatSoftwareDevelopment/Sprint03_EmApplicationDefinition/issue_72_yaml2nxdl/NXmx.yml'
+fnm = '/home/kuehbach/HU_HU_HU/FAIRmatSoftwareDevelopment/Sprint03_EmApplicationDefinition/issue_72_yaml2nxdl/NXarpes.yml'
+
 with open(fnm) as stream:
     try:
         yml = yaml.safe_load(stream)
@@ -36,10 +38,16 @@ nx_base_clss = ['nx_aperture', 'nx_attenuator', 'nx_beam', 'nx_beam_stop', 'nx_b
     'nx_positioner', 'nx_process', 'nx_reflections', 'nx_root', 'nx_sample', 'nx_sample_component', 'nx_sensor', 'nx_shape', 
     'nx_slit', 'nx_source', 'nx_subentry', 'nx_transformations', 'nx_translation', 'nx_user', 'nx_velocity_selector',
     'nx_xraylens']
-nx_type_keys = ['nx_date_time', 'nx_char', 'nx_number', 'nx_float', 'nx_uint', 'nx_int', 'nx_boolean']
+nx_type_keys = ['nx_binary', 'nx_boolean', 'nx_char', 'nx_date_time', 
+                'nx_float', 'nx_int', 'nx_number', 'nx_posint', 'nx_uint']
 nx_attr_idnt = '\@'
 nx_unit_idnt = 'units'
-
+nx_unit_catg = ['nx_angle',  'nx_any', 'nx_area', 'nx_charge', 'nx_cross_section', 'nx_current', 'nx_dimensionless',
+                'nx_emittance', 'nx_energy', 'nx_flux', 'nx_frequency', 'nx_length', 'nx_mass', 'nx_mass_density',
+                'nx_molecular_weight', 'nx_period', 'nx_per_area', 'nx_per_length', 'nx_power', 'nx_pressure',
+                'nx_pulses', 'nx_scattering_length_density', 'nx_solid_angle', 'nx_temperature', 'nx_time',
+                'nx_time_of_flight', 'nx_transformation', 'nx_unitless', 'nx_voltage', 'nx_volume', 'nx_wavelength',
+                'nx_wavenumber']
 
 def recursive_build(obj, dict_value):
     """
@@ -47,15 +55,27 @@ def recursive_build(obj, dict_value):
     dctv is value for a key in a (nested) dictionary
     """
     for k, v in iter(dict_value.items()):
-        print('Processing '+k)
-        if not isinstance(v, dict): #if value v is not a dictionary, we should be in the definitions part of the NXDL schema
+        print('Processing key '+k+' value v is a dictionary '+str(isinstance(v,dict)))
+        if not isinstance(v, dict): #if value v is not a dictionary, we should be in the definitions part of the NXDL schema, or a field 
             if k[0:2] == nx_attr_idnt: #if we have an attribute
                 attr = etree.SubElement(obj, 'attribute')
                 attr.set('name', k[1:])
                 ###MK::start a new recursion ??
+            elif v == None:
+                print('Dangling entry '+k) #this could be an individually dangling item, maybe a field such as <field name="title" type="NX_CHAR"/> or a group, maybe a base class
+                if k in nx_base_clss:
+                    grp = etree.SubElement(obj, 'group')
+                    grp.set('name', nx_base_clss_string_mangling(k))
+                else: ###MK::assume it is a field
+                    fld = etree.SubElement(obj, 'field')
+                    fld.set('name', k)
+                #either way, do not start a new recursion because there are no associated nested pieces of information
             else: #is not an attribute
                 if k == 'name': #is key a name?
-                    obj.set('name', v)
+                    if v != None:
+                        obj.set('name', v)
+                    else:
+                        obj.set('name', k)
                 elif k == 'type': #is key a type?
                     if v in nx_type_keys:
                         obj.set(k, v.upper())
@@ -69,6 +89,7 @@ def recursive_build(obj, dict_value):
                     obj.set('doc', v)
                 elif k in nx_base_clss: #is key an instance of an nx_base_clss?
                     print('WARNING: '+k+' is a base class but value v is not a dictionary !')
+                    print('WARNING: '+k+' this indicates there is eventually an inconsistence in your specification, please inspect it further')
                 elif k == nx_unit_idnt:
                     obj.set('units', v.upper())
                 elif k == 'exists':
@@ -90,7 +111,6 @@ def recursive_build(obj, dict_value):
                             itm.set('value', str(m))
                     else:
                         raise ValueError('ERROR: '+k+' we found an enumeration key-value pair but the value is not an ordinary Python list !')
-                #
                 elif k == 'rank':
                     obj.set('rank', str(v))
                 elif k == 'dim':
@@ -110,7 +130,8 @@ def recursive_build(obj, dict_value):
                                     raise ValueError('WARNING: '+k+' dimensions list item needs to have at least two members !' )
                     else:
                         raise ValueError('ERROR: '+k+' we found an dimensions key-value pair but the value is not an ordinary Python list of list !')
-                #
+                elif k == None:
+                    obj.set('name', k)
                 elif v == None:
                     obj.set('name', k)
                 else:
@@ -167,117 +188,3 @@ recursive_build(rt, yml)
 nxdl = etree.ElementTree(rt)
 nxdl.write( fnm + '.nxdl', pretty_print=True, xml_declaration=True, encoding="utf-8" )
 print('Parsed YAML to NXDL')
-
-
-
-##recursive_build(rt, yml['magic'])
-##entry = etree.SubElement(rt, 'nx_entry') #'group'
-##recursive_build(entry, yml['nx_entry'])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# =============================================================================
-# def is_leaf(pth, dct):
-#     """
-#     checks if path pth in a dictionary dct is a leaf, i.e. a value which is no longer a dictionary
-#     """
-#     pth = 'entry1/mygroup'
-#     #tmpdct = dct
-#     nodes = pth.split('/') #nodes along the path to resolve nested dictionaries
-#     currpath = ''
-#     #is_leaf = True #argue by falsification
-#     for i in nodes:
-#         currpath += '/' + i
-#         #currpath = '/entry1'
-#         print(currpath[1::])
-#         if currpath[1::] in tmpdct:
-#             tmpdct = tmpdct[i]
-#             if currpath[1::] != pth:
-#                 continue #traverse down
-#             else:
-#                 #arrived at the target path
-#                 for k, v in tmpdct[i].items():
-#                     if type(v) == dict:
-#                         is_leaf = False
-#                         print('False')
-#                 print('True')
-#         else:
-#             raise ValueError('Path ' + currpath[1::] +' is not resolvable for dictionary !')
-#             
-# a = is_leaf('entry1/my_group', yml)
-#         
-# 
-# class yml2nxdl():
-#     def __init__(self, fnm, *args, **kwargs):
-#         self.yml = {}
-#         self.rt = None
-#         self.fnm = fnm
-#         with open(self.fnm) as stream:
-#             try:
-#                 self.yml = yaml.safe_load(stream)
-#                 print(self.yml)
-#                 rt = etree.Element('description')
-#             except yaml.YAMLError as exc:
-#                 print(exc)
-#     
-#     def set_root(self):
-#         if 'magic' in yml:
-#             if 'name' in yml['magic']:
-#                 rt.set("name", yml['magic']['name'])
-#             if 'doc' in yml['magic']:
-#                 rt.set("doc", yml['magic']['doc'])
-#             else:
-#                 #MK::accept default for NXobject
-#                 pass
-#             
-#     def set_entry(self):
-#         if len(yml) == 2:
-#             
-#             pass
-#         else:
-#             raise ValueError('Current implementation supports that there is only one NXentry group')
-#                 
-#     def write_nxdl(self):
-#         nxdl = etree.ElementTree(rt)
-#         nxdl.write( fnm + '.nxdl', pretty_print=True, xml_declaration=True, encoding="utf-8" )
-# 
-#                 
-# for k in yml[entry].keys():
-#     if not isinstance(yml[k],dict):
-#         
-#             
-# rt = etree.Element('description')
-# #tmp = etree.Element('doc')
-# nxdl = etree.ElementTree(rt)
-# nxdl.write( fnm + '.nxdl', pretty_print=True, xml_declaration=True, encoding="utf-8" )
-# 
-# 
-# rt = etree.Element('description')
-# if 'magic' in yml:
-#     if 'name' in yml['magic']:
-#         rt.set("name", yml['magic']['name'])
-#     if 'doc' in yml['magic']:
-#         rt.set("doc", yml['magic']['doc'])
-#     else:
-#         #MK::accept default for NXobject
-#         pass
-#     
-# if 'entry' in yml:
-#     if isinstance(yml['entry'], dict):
-# =============================================================================
-
-#TO DO:
-#stylesheet information missing
-#<?xml-stylesheet type="text/xsl" href="nxdlformat.xsl" ?>
