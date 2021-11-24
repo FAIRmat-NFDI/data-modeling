9e7ca68cd175d9675476ba43a51ce2d9e2817d36
Markus Kuehbach <kuehbach@fhi-berlin.mpg.de>
2021-11-24 18:25:54 +0000

    updated yaml2nxdl_utils.py

--- yaml-to-nxdl-converter/yaml2nxdl_utils.py
+++ yaml-to-nxdl-converter/yaml2nxdl_utils.py
@@ -21,6 +21,27 @@ def nx_base_clss_string_mangling(tmp):
         return tmp
         #raise ValueError(tmp+' is not a properly formatted nexus keyword which this parser knows!')
 
+def nx_name_type_resolving(tmp):
+    """
+    extracts the type and eventually custom name from a YML section string
+    """
+    #tmp = '' #will throw
+    #tmp = '(NXentry)' #will return '', 'NXentry' tuple
+    #tmp = 'condensor(NXem_lens)' #will return 'condensor', 'NXem_lens' tuple
+    #tmp = 'type'
+    if tmp.count('(') == 1 and tmp.count(')') == 1: 
+        #we can safely assume that every valid YML key resolves
+        #either an nx_ (type, base, candidate) class contains only 1 '(' and ')'
+        idxStart = tmp.index('(')
+        idxEnd = tmp.index(')',idxStart+1)
+        typ = tmp[idxStart+1:idxEnd] #.replace('NX','nx_')
+        nam = tmp.replace('('+typ+')','') #.replace('nx_','NX')+')','')
+        return nam, typ
+    #or a name for a member
+    typ = ''
+    nam = tmp
+    return nam, typ
+
 
 nx_base_clss = ['nx_aperture', 'nx_attenuator', 'nx_beam', 'nx_beam_stop', 'nx_bending_magnet', 'nx_capillary', 'nx_cite', 
     'nx_collection', 'nx_collimator', 'nx_crystal', 'nx_cylindrical_geometry', 'nx_data', 'nx_detector', 
@@ -34,10 +55,10 @@ nx_base_clss = ['nx_aperture', 'nx_attenuator', 'nx_beam', 'nx_beam_stop', 'nx_b
 
 nx_type_keys = ['nx_binary', 'nx_boolean', 'nx_char', 'nx_date_time', 
                 'nx_float', 'nx_int', 'nx_number', 'nx_posint', 'nx_uint']
-nx_grpnm_tag = '___'
+#nx_grpnm_tag = '___'
 nx_attr_idnt = '\@'
-nx_unit_idnt = 'units'
-nx_unit_catg = ['nx_angle',  'nx_any', 'nx_area', 'nx_charge', 'nx_cross_section', 'nx_current', 'nx_dimensionless',
+nx_unit_idnt = 'unit'
+nx_unit_typs = ['nx_angle',  'nx_any', 'nx_area', 'nx_charge', 'nx_cross_section', 'nx_current', 'nx_dimensionless',
                 'nx_emittance', 'nx_energy', 'nx_flux', 'nx_frequency', 'nx_length', 'nx_mass', 'nx_mass_density',
                 'nx_molecular_weight', 'nx_period', 'nx_per_area', 'nx_per_length', 'nx_power', 'nx_pressure',
                 'nx_pulses', 'nx_scattering_length_density', 'nx_solid_angle', 'nx_temperature', 'nx_time',
