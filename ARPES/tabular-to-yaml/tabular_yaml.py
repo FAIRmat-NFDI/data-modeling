aa3479e968432c0ddf406e48c8e4ddc6a7808ce8
kanbina <yolanne.lee95@gmail.com>
2021-12-03 12:05:53 +0000

    YAML to xlsx fixed according to new YAML format.

--- ARPES/tabular-to-yaml/tabular_yaml.py
+++ ARPES/tabular-to-yaml/tabular_yaml.py
@@ -1,17 +1,20 @@
 import pandas as pd, numpy as np, yaml, pprint, argparse, re, os
 from pandas import ExcelWriter
 
-class Dictlist(dict):
-    def __setitem__(self, key, value):
-        try:
-            self[key]
-        except KeyError:
-            super(Dictlist, self).__setitem__(key, [])
-        self[key].append(value)
+"""
+Tabular-YAML two-way conversion script for ARPES definitions
+Required arguments: -n <filename> -d <destination folder>
+
+Example conversion from xlsx to yaml:
+    ./tabular_yaml.py -n mpes-nexus_metadata_parameters_reviewed.xlsx -d testing
+
+Example conversion from yaml to xlsx:
+    ./tabular_yaml.py -n testing/NXsample_converted.yaml -d to_xlsx
+"""
 
 
-# filename = 'mpes-nexus_metadata_parameters.xlsx'
-def csv_to_yaml(args):
+def csv_to_yaml():
+    print("Not fully tested")
     yaml_filename = args.filename.replace(args.filename.split(".", 1)[1], 'yaml')
     temp_df = pd.read_csv(args.filename)
     temp_df.dropna(inplace=True)
@@ -52,7 +55,6 @@ def cleanDF(df):
     
     return df
 
-
 def clean_duplicate_handler(filepath):
     with open(filepath, "r") as text_file:
         data = text_file.read()
@@ -64,18 +66,10 @@ def clean_duplicate_handler(filepath):
 
     text_file.close()
 
-def xlsx_to_yaml(args):
-    # yaml_filename = filename.split(".", 1)[0]+"_converted.yaml"
-    # print(yaml_filename)
-    # yaml_filename = filename.replace(filename, 'yaml')
-    xls = pd.ExcelFile(args.filename)
+def xlsx_to_yaml():
+    xls = pd.ExcelFile(file_path)
     names = xls.sheet_names
-    names = ["NXsample"] 
-    if not os.path.exists(args.destination):
-        os.makedirs(args.destination)
-    
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    folder_path = os.path.join(dir_path,args.destination)
+    # names = ["NXsample"] 
 
     for sheet in names:
         print("Processing sheet: " + sheet)
@@ -85,12 +79,11 @@ def xlsx_to_yaml(args):
         diff = len(temp_df)-len(df)
 
         if(diff != 0):
-            print("WARNING: One or more column(s) had the same name and only the first was kept.")
+            print("WARNING: One or more column(s) had the same nexus hierarchy and only the first was kept.\nThis can happen if your file has merged rows.")
             print("     Sheet:" + sheet)
             print("     Number of rows dropped:" + str(diff))
 
         df = cleanDF(df)
-        print(df)
         sheet_dict = df.to_dict('index')
         sheet_dict = {
             outer_k: {
@@ -101,8 +94,7 @@ def xlsx_to_yaml(args):
             for outer_k, outer_v in sheet_dict.items()
         }
 
-        pprint.pprint(sheet_dict)
-        file_path = os.path.join(folder_path,yaml_filename)
+        # file_path = os.path.join(folder_path,yaml_filename)
         output={}
         tempstring = "(" + sheet + ")"
         output[tempstring] = sheet_dict
@@ -110,53 +102,69 @@ def xlsx_to_yaml(args):
             yaml.dump(output, f)
 
         clean_duplicate_handler(file_path)
-        # master[sheet] = sheet_dict
-
-    # pprint.pprint(master)
-
-    # with open(yaml_filename, "w") as f:
-    #     yaml.dump(master, f)
     
     print("The new file has been saved as " + yaml_filename)
 
-def to_xlsx(args):
+def to_xlsx():
     # print("to be done")
-    xlsx_filename = args.filename.split(".", 1)[0]+"_converted.xlsx"
-    with open(args.filename, 'r') as stream:
+    with open(file_path, 'r') as stream:
         try:
             parsed_yaml=yaml.safe_load(stream)
-            writer = ExcelWriter(xlsx_filename)
             # pprint.pprint(parsed_yaml)
             for key in parsed_yaml:
                 print(key)
+                xlsx_filename = str(key).replace("(","").replace(")","") + "_converted.xlsx"
+                outputfile = os.path.join(folder_path,xlsx_filename)
+                writer = ExcelWriter(outputfile)
                 one_sheet = parsed_yaml[key]
                 holder = []
                 names = []
+                types = []
                 for row in one_sheet:
                     values = one_sheet[row]
                     df = pd.DataFrame(data=values,index = [0])
                     # print("asdfasdf")
+                    nexusheirarchy = row.split(" ")[0]
+                    type = row.split(" ",1)[1].replace("(","").replace(")","")
+                    # print(nexusheirarchy)
+                    # print(type)
                     # print(df)
+                    # print(names)
                     holder.append(df)
-                    names.append(str(row))
+                    names.append(nexusheirarchy)
+                    types.append(type)
                 output = pd.concat(holder)
-                output.insert(loc=0, column='Name', value=names)
+                output.insert(loc=0, column='Nexus heirarchy', value=names)
+                output.insert(loc=1, column='Type', value=types)
+                output['Dim'] = output['Dim'].fillna(0)
+                output['Rank'] = output['Rank'].fillna(0)
                 output.to_excel(writer,key,index=False)
 
             writer.save()
         except yaml.YAMLError as exc:
             print(exc)
 
-
 def main(args):
     print("Parameter file yaml-tabular parser.")
+    if not os.path.exists(args.destination):
+        os.makedirs(args.destination)
+    
+    args.filename = os.path.join(*args.filename.split("/"))
+    # dir_path = os.path.dirname(__file__)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    global folder_path
+    global file_path
+    folder_path = os.path.join(dir_path,args.destination)
+    file_path = os.path.join(dir_path,args.filename)
+    print("outputfolder_path: " + folder_path)
+    print("readfile_path: " + file_path)
 
     if args.filename.split(".", 1)[1] == "xlsx":
-        xlsx_to_yaml(args)
+        xlsx_to_yaml()
     elif args.filename.split(".", 1)[1] == "csv":
-        csv_to_yaml(args)
+        csv_to_yaml()
     elif args.filename.split(".", 1)[1] == "yaml":
-        to_xlsx(args)
+        to_xlsx()
 
 
 if __name__ == "__main__":
@@ -164,9 +172,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Optional argument which requires a parameter (eg. -d test)
-    parser.add_argument("-n", "--filename", action="store", dest="filename", help="filename")
+    parser.add_argument("-n", "--filename", action="store", dest="filename", help="filename", required=True)
 
-    parser.add_argument("-d", "--dest", action="store", dest="destination", help="Folder in which to save output files")
+    parser.add_argument("-d", "--dest", action="store", dest="destination", help="Folder in which to save output files", required=True)
 
     args = parser.parse_args()
     main(args)
