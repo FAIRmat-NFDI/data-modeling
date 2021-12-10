Fixed units; added header info.

ARPES/tabular-to-yaml/tabular_yaml.py
@@ -1,5 +1,11 @@
-import pandas as pd, numpy as np, yaml, pprint, argparse, re, os
-from pandas.core.dtypes.missing import isnull
+import pandas as pd
+import numpy as np
+import yaml
+import pprint
+import argparse
+import re
+import os
+# from pandas.core.dtypes.missing import isnull
 from pandas import ExcelWriter
 from openpyxl import load_workbook
 
@@ -20,9 +26,8 @@ def csv_to_yaml():
     yaml_filename = args.filename.replace(args.filename.split(".", 1)[1], 'yaml')
     temp_df = pd.read_csv(args.filename)
     temp_df.dropna(inplace=True)
-    df = temp_df.drop_duplicates(subset=['Name'],inplace=False)
+    df = temp_df.drop_duplicates(subset=['Name'], inplace=False)
     diff = len(temp_df)-len(df)
-
     if(diff != 0):
         print("WARNING: One or more column(s) had the same name and only the first was kept.")
         print("     Number of rows dropped:" + str(diff))
@@ -32,8 +37,10 @@ def csv_to_yaml():
     with open(yaml_filename, "w") as f:
         yaml.dump(sheet_dict, f)
 
+
 def stripspaces(string):
     return re.sub(r"\s+", "", str(string), flags=re.UNICODE)
+
 
 def cleanDF(df):
     df = df.fillna('')
@@ -41,48 +48,53 @@ def cleanDF(df):
     MergedTypeClass = df['type:units'].str.split(':').str[0].apply(stripspaces) + df['NX class'].astype(str).apply(stripspaces)
     df['Name'] = df['Nexus hierarchy'].apply(stripspaces) + "(" + MergedTypeClass + ")"
     df = df[df.Name != "()"]
-    df['unit'] = df['type:units'].str.split(':').str[-1].apply(stripspaces)
-    df['doc'] = df['Documentation'].replace('', np.nan, inplace=False)
-    df.drop(columns=["Nexus hierarchy","type:units","NX class","Documentation"], inplace = True)
+    units = [stripspaces(s.split(':')[-1]) if len(s.split(':')) > 1 else "" for s in df['type:units']]
+    df['unit'] = units
+    df['doc'] = "\"" + df['Documentation'].replace('', np.nan, inplace=False) + "\""
+    df.drop(columns=["Nexus hierarchy", "type:units", "NX class", "Documentation"], inplace=True)
     df.set_index('Name', inplace=True)
-    df.dropna(axis=0,thresh=3,inplace=True)
-    df.dropna(axis=1,thresh=3,inplace=True)
+    df.dropna(axis=0, thresh=3, inplace=True)
+    df.dropna(axis=1, thresh=3, inplace=True)
     check_dupes = df.index.duplicated(keep='first')
     num = 0
     while any(check_dupes):
-        new_indices = [str(s) if check_dupes[ind] == False else (str(s) + "_dupe" + str(num)) for ind, s in enumerate(df.index)]
-        num+=1
+        new_indices = [str(s) if not check_dupes[ind] else (str(s) + "_dupe" + str(num)) for ind, s in enumerate(df.index)]
+        num += 1
         df.index = new_indices
         check_dupes = df.index.duplicated(keep='first')
 
     return df
 
+
 def clean_duplicate_handler(filepath):
-    with open(filepath, "r",errors='ignore') as text_file:
+    with open(filepath, "r", errors='ignore') as text_file:
         data = text_file.read()
         # print(re.findall(r'_dupe\w+',data))
         data = re.sub(r'_dupe\w+', "", data)
         data = re.sub(r'\'\[\[', "[[", data)
         data = re.sub(r'\]\]\'', "]]", data)
+        data = re.sub(r'\'\"', "\"", data)
+        data = re.sub(r'\"\'', "\"", data)
 
     with open(filepath, "w") as text_file:
         text_file.write(data)
 
     text_file.close()
 
+
 def xlsx_to_yaml():
     # xls = pd.ExcelFile(file_path,engine='openpyxl')
-    xls = load_workbook(file_path,read_only=True,data_only=True)
+    xls = load_workbook(file_path, read_only=True, data_only=True)
     names = xls.sheetnames
     # names = ["NXsample"]
 
     for sheet in names:
         print("Processing sheet: " + sheet)
-        #yaml_filename = sheet + "_converted.yaml"
+        # yaml_filename = sheet + "_converted.yaml"
         yaml_filename = sheet + ".yaml"
         ws = xls[sheet]
-        temp_df = pd.DataFrame(ws.values, columns = next(ws.values)[0:])
-        df = temp_df.drop_duplicates(subset=['Nexus hierarchy'],inplace=False).iloc[1: , :]
+        temp_df = pd.DataFrame(ws.values, columns=next(ws.values)[0:])
+        df = temp_df.drop_duplicates(subset=['Nexus hierarchy'], inplace=False).iloc[1:, :]
         diff = len(temp_df)-len(df)
 
         if(diff != 0):
@@ -107,16 +119,28 @@ def xlsx_to_yaml():
                 rank = sheet_dict.get(key).pop("Rank")
                 if dim != 0:
                     dim = "[" + str(dim) + "]"
-                    newitem = {"dimensions": {"dim":dim, "rank":rank}}
+                    newitem = {"dimensions": {"dim": dim, "rank": rank}}
                     sheet_dict.get(key).update(newitem)
 
         # pprint.pprint(sheet_dict)
-        output_path = os.path.join(folder_path,yaml_filename)
-        output={}
-        tempstring = "(" + sheet + ")"
-        output[tempstring] = sheet_dict
+        output_path = os.path.join(folder_path, yaml_filename)
+        output = {}
+        with open("Headers.yaml", 'r') as stream:
+            try:
+                parsed_yaml = yaml.safe_load(stream)
+                if(sheet in parsed_yaml):
+                    if 'doc' in parsed_yaml[sheet]:
+                        parsed_yaml[sheet].update({'doc': "\"" + str(parsed_yaml[sheet]['doc']) + "\""})
+                    output.update(parsed_yaml[sheet])
+                    output['(NXobject)'] = sheet_dict
+                else:
+                    print("ERROR: Not found in Headers.yaml; defaulting to no header information.")
+                    tempstring = "(" + sheet + ")"
+                    output[tempstring] = sheet_dict
+            except yaml.YAMLError as exc:
+                print(exc)
         with open(output_path, "w") as f:
-            yaml.dump(output, f)
+            yaml.dump(output, f, sort_keys=False)
             f.close()
 
         clean_duplicate_handler(output_path)
@@ -124,16 +148,17 @@ def xlsx_to_yaml():
     xls.close()
     print("The new file has been saved as " + yaml_filename)
 
+
 def to_xlsx():
     # print("to be done")
     with open(file_path, 'r') as stream:
         try:
-            parsed_yaml=yaml.safe_load(stream)
+            parsed_yaml = yaml.safe_load(stream)
             # pprint.pprint(parsed_yaml)
             for key in parsed_yaml:
                 print(key)
-                xlsx_filename = str(key).replace("(","").replace(")","") + "_converted.xlsx"
-                outputfile = os.path.join(folder_path,xlsx_filename)
+                xlsx_filename = str(key).replace("(", "").replace(")", "") + "_converted.xlsx"
+                outputfile = os.path.join(folder_path, xlsx_filename)
                 writer = ExcelWriter(outputfile)
                 one_sheet = parsed_yaml[key]
                 holder = []
@@ -141,10 +166,10 @@ def to_xlsx():
                 types = []
                 for row in one_sheet:
                     values = one_sheet[row]
-                    df = pd.DataFrame(data=values,index = [0])
+                    df = pd.DataFrame(data=values, index=[0])
                     # print("asdfasdf")
                     nexusheirarchy = row.split(" ")[0]
-                    type = row.split(" ",1)[1].replace("(","").replace(")","")
+                    type = row.split(" ", 1)[1].replace("(", "").replace(")", "")
                     # print(nexusheirarchy)
                     # print(type)
                     # print(df)
@@ -157,11 +182,12 @@ def to_xlsx():
                 output.insert(loc=1, column='Type', value=types)
                 output['Dim'] = output['Dim'].fillna(0)
                 output['Rank'] = output['Rank'].fillna(0)
-                output.to_excel(writer,key,index=False)
+                output.to_excel(writer, key, index=False)
 
             writer.save()
         except yaml.YAMLError as exc:
             print(exc)
+
 
 def main(args):
     print("Parameter file yaml-tabular parser.")
@@ -173,8 +199,8 @@ def main(args):
     dir_path = os.path.dirname(os.path.realpath(__file__))
     global folder_path
     global file_path
-    folder_path = os.path.join(dir_path,args.destination)
-    file_path = os.path.join(dir_path,args.filename)
+    folder_path = os.path.join(dir_path, args.destination)
+    file_path = os.path.join(dir_path, args.filename)
     print("outputfolder_path: " + folder_path)
     print("readfile_path: " + file_path)
 